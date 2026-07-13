7079260901178887784bdd022e8338bed496e74c
Mughees Khan <khanmughees587@gmail.com>
2026-07-13 23:44:59 +0500

    Add files via upload

--- client/start_client.sh
+++ client/start_client.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
+#
+# PROTO env var selects which config to run:
+#   PROTO=wpa2 (default) -> wpa_supplicant.conf      (WPA2-PSK)
+#   PROTO=wpa3           -> wpa_supplicant_wpa3.conf  (WPA3-SAE)
+#
+PROTO="${PROTO:-wpa2}"
 
-echo "[CLIENT] Starting WPA2 Client..."
+if [ "$PROTO" = "wpa3" ]; then
+    CONF_SRC="/etc/wpa_supplicant/wpa_supplicant_wpa3.conf"
+    LABEL="WPA3-SAE"
+else
+    CONF_SRC="/etc/wpa_supplicant/wpa_supplicant.conf"
+    LABEL="WPA2-PSK"
+fi
+
+echo "[CLIENT] Starting $LABEL Client..."
 echo "[CLIENT] Using interface: $IFACE"
+echo "[CLIENT] Using config: $CONF_SRC"
 
 ip link set $IFACE up
 
@@ -11,11 +26,10 @@ sleep 1
 echo "[CLIENT] Waiting for AP to be ready..."
 sleep 8
 
-echo "[CLIENT] Launching wpa_supplicant..."
-sed -i "s/wlan1/$IFACE/" /etc/wpa_supplicant/wpa_supplicant.conf
+echo "[CLIENT] Launching wpa_supplicant ($LABEL)..."
 wpa_supplicant -i $IFACE \
     -D nl80211 \
-    -c /etc/wpa_supplicant/wpa_supplicant.conf \
+    -c "$CONF_SRC" \
     -d 2>&1 | tee /shared/client_log.txt
 
 echo "[CLIENT] Done"
