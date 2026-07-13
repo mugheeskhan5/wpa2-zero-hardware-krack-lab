392761539e8ed91402f44d8ef5f3b4de06c45ba7
Mughees Khan <khanmughees587@gmail.com>
2026-07-13 23:43:59 +0500

    Add files via upload

--- ap/start_ap.sh
+++ ap/start_ap.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
+#
+# PROTO env var selects which config to run:
+#   PROTO=wpa2 (default) -> hostapd.conf       (WPA2-PSK)
+#   PROTO=wpa3           -> hostapd_wpa3.conf   (WPA3-SAE)
+#
+PROTO="${PROTO:-wpa2}"
 
-echo "[AP] Starting WPA2 Access Point..."
+if [ "$PROTO" = "wpa3" ]; then
+    CONF_SRC="/etc/hostapd/hostapd_wpa3.conf"
+    LABEL="WPA3-SAE"
+else
+    CONF_SRC="/etc/hostapd/hostapd.conf"
+    LABEL="WPA2-PSK"
+fi
+
+echo "[AP] Starting $LABEL Access Point..."
 echo "[AP] Using interface: $IFACE"
+echo "[AP] Using config: $CONF_SRC"
 
 ip link set $IFACE up
-sed -i "s/interface=wlan0/interface=$IFACE/" /etc/hostapd/hostapd.conf
+sed -i "s/interface=wlan0/interface=$IFACE/" "$CONF_SRC"
 
 tcpdump -i $IFACE -w /shared/capture.pcap "ether proto 0x888e" &
 echo "[AP] tcpdump capturing on $IFACE..."
 
 sleep 2
 
-echo "[AP] Launching hostapd..."
-hostapd /etc/hostapd/hostapd.conf
+echo "[AP] Launching hostapd ($LABEL)..."
+hostapd "$CONF_SRC"
 
 echo "[AP] Done"
